ab175eec8763886b2ffb9c7b60eb7ad697033cf4
ramSeraph <kandimalla.sreeram@gmail.com>
2024-12-28 18:07:52 +0530

    some cleanup in the capabilities test

--- tests/test_capabilities.py
+++ tests/test_capabilities.py
@@ -30,42 +30,55 @@ class TestGeorssParsing(TestCase):
     def test_wms_capabilities_bug(self):
         expected = self.load_json_file('wms_capabilities.json')
         xml_txt = self.load_file('wms_capabilities.xml')
+
         layer_list = []
         service_info = {}
         parse_capabilities('WMS', xml_txt, layer_list, service_info)
-        #Path('out.json').write_text(json.dumps({ 'service_info': service_info, 'layer_list': layer_list }))
+
         self.assertEqual(layer_list, expected['layer_list'])
         self.assertEqual(service_info, expected['service_info'])
 
     def test_wms_capabilities_sax_cleanup_bug(self):
         expected = self.load_json_file('wms_capabilities_sax_cleanup_bug.json')
         xml_txt = self.load_file('wms_capabilities_sax_cleanup_bug.xml')
+
         layer_list = []
         service_info = {}
         parse_capabilities('WMS', xml_txt, layer_list, service_info)
-        #Path('out.json').write_text(json.dumps({ 'service_info': service_info, 'layer_list': layer_list }))
+
         self.assertEqual(layer_list, expected['layer_list'])
         self.assertEqual(service_info, expected['service_info'])
 
     def test_wms_capabilities_incomplete(self):
         expected = self.load_json_file('wms_capabilities_incomplete.json')
         xml_txt = self.load_file('wms_capabilities_incomplete.xml')
+
         layer_list = []
         service_info = {}
         with self.assertRaises(Exception):
             parse_capabilities('WMS', xml_txt, layer_list, service_info)
+
         self.assertEqual(layer_list, expected['layer_list'])
         self.assertEqual(service_info, expected['service_info'])
 
     def test_wfs_capabilities(self):
         expected = self.load_json_file('wfs_capabilities.json')
         xml_txt = self.load_file('wfs_capabilities.xml')
+
         layer_list = []
         service_info = {}
         parse_capabilities('WFS', xml_txt, layer_list, service_info)
+
         self.assertEqual(layer_list, expected['layer_list'])
         self.assertEqual(service_info, expected['service_info'])
 
     def test_wfs_disabled_ows_namespace(self):
         xml_txt = self.load_file('wfs_disabled_ows_namespace.xml')
-        self.assertRaises(WFSUnsupportedException, parse_capabilities, 'WFS', xml_txt, [], {})
+
+        layer_list = []
+        service_info = {}
+        with self.assertRaises(WFSUnsupportedException):
+            parse_capabilities('WFS', xml_txt, layer_list, service_info)
+
+        self.assertEqual(layer_list, [])
+        self.assertEqual(service_info, {})
